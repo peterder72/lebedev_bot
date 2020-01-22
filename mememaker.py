c7111b17e3aa73c127cd51fd28932856114d79b9
Deriabin,Peter P <p.deriabin@student.fontys.nl>
2020-01-22 21:20:06 +0100

    Added overflow protection for text

--- mememaker.py
+++ mememaker.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageDraw, ImageFont
 
 def create_lebedev(verb: str) -> Image:
 
-    template = f'Ну {verb} и {verb}'
+    template_phrase = ((f'Ну {verb}'), (f'и {verb}'))
 
     base = Image.open("img/lebedev_base.jpeg")
 
@@ -13,22 +13,23 @@ def create_lebedev(verb: str) -> Image:
 
     y_offset = 30
 
-    size = draw.multiline_textsize(template, font=font, spacing=4)
+    text = ' '.join(template_phrase)
+
+    size = draw.multiline_textsize(text, font=font, spacing=4)
 
     if size[0] > base.size[0]:
-        template = '\n'.join(
-            [
-                ' '.join(template.split(' ')[:2]),
-                ' '.join(template.split(' ')[2:])
-            ]
-        )
-        size = draw.multiline_textsize(template, font=font, spacing=8)
+        text = '\n'.join(template_phrase)
+        size = draw.multiline_textsize(text, font=font, spacing=8)
+
+        if size[0] > base.size[0]:
+            googly_eyes = Image.open("img/googly_eyes.png")
+            base.paste(googly_eyes, (0, 0), mask=googly_eyes)
 
     x_coord = (base.size[0] - size[0]) // 2
 
     coord = (x_coord, base.size[1] - size[1] - y_offset)
 
-    draw.multiline_text(coord, template, fill=color, font=font, spacing=4)
+    draw.multiline_text(coord, text, fill=color, font=font, spacing=8)
 
     return base
 
